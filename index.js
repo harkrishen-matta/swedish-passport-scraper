8801087313ab2d9ddce3cc3f36f8c0d8840ea806
Harkrishen Matta <zdac105@live.rhul.ac.uk>
2022-05-27 16:40:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const PassportScraper = require('./passportScraper')
+
+app.get('/', (req, res) => {
+    PassportScraper().then(v =>res.send(v))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ passportScraper.js
@@ -0,0 +1,48 @@
+const puppeteer = require('puppeteer');
+
+const PassportScraper = async() => {
+  const browser = await puppeteer.launch({ headless: true});
+  const page = await browser.newPage();
+  await page.goto('https://www.migrationsverket.se/ansokanbokning/valjtyp?0&enhet=U0586&sprak=sv&callback=https:/www.swedenabroad.se');
+
+  //await page.select("select#viseringstyp", "ansöka om svenskt pass/id-handlingar")
+  await page.click('select#viseringstyp');
+  await page.keyboard.press('ArrowDown');
+  await page.keyboard.press('ArrowDown');
+  await page.keyboard.press('Enter');
+
+  await page.waitForSelector('#antalpersoner', {visible: true})
+  await page.select("select#antalpersoner", "0")
+
+  await page.waitFor(500)
+
+  const checkbox = await page.$('input[name="control.panel:control.checkbox"]');
+  await checkbox.click()
+
+  await page.waitFor(1000)
+
+  const submit = await page.$('input[name="fortsatt"]');
+  await submit.click()
+
+  await page.waitFor(1000)
+
+  var appointmentAvailability = ''
+  if ((await page.content()).match('Det finns inga mer lediga tider för närvarande.') !== null) {
+    appointmentAvailability = 'No Appointments Available'
+    console.log('No Appointments Available')
+  }
+  else {
+    appointmentAvailability = 'Appointments Available'
+    console.log('Appointments Available')
+
+  }
+  
+  await page.waitFor(1000)
+
+  await browser.close();
+
+  return appointmentAvailability;
+
+}
+
+module.exports = PassportScraper;
