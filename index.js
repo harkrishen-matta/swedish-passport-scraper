7d8b3a5a794368711aaaff0cda88119155d24082
Harkrishen Matta <zdac105@live.rhul.ac.uk>
2022-05-30 13:47:33 +0100

    Added Prcofile

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const PassportScraper = require('./passportScraper')
+const express = require("express");
+const app = express();
+const port = 3000;
+const PassportScraper = require("./passportScraper");
 
-app.get('/', (req, res) => {
-    PassportScraper().then(v =>res.send(v))
-})
+app.get("/", (req, res) => {
+  PassportScraper()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) =>
+      res.status(500).send({
+        message: "Failure",
+        errorMessage: err.message,
+      })
+    );
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- passportScraper.js
+++ passportScraper.js
@@ -1,48 +1,62 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-const PassportScraper = async() => {
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox']});
-  const page = await browser.newPage();
-  await page.goto('https://www.migrationsverket.se/ansokanbokning/valjtyp?0&enhet=U0586&sprak=sv&callback=https:/www.swedenabroad.se');
+const PassportScraper = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox"],
+      });
+      const page = await browser.newPage();
+      await page.goto(
+        "https://www.migrationsverket.se/ansokanbokning/valjtyp?0&enhet=U0586&sprak=sv&callback=https:/www.swedenabroad.se"
+      );
 
-  //await page.select("select#viseringstyp", "ansöka om svenskt pass/id-handlingar")
-  await page.click('select#viseringstyp');
-  await page.keyboard.press('ArrowDown');
-  await page.keyboard.press('ArrowDown');
-  await page.keyboard.press('Enter');
+      //await page.select("select#viseringstyp", "ansöka om svenskt pass/id-handlingar")
+      await page.click("select#viseringstyp");
+      await page.keyboard.press("ArrowDown");
+      await page.keyboard.press("ArrowDown");
+      await page.keyboard.press("Enter");
 
-  await page.waitForSelector('#antalpersoner', {visible: true})
-  await page.select("select#antalpersoner", "0")
+      await page.waitForSelector("#antalpersoner", { visible: true });
+      await page.select("select#antalpersoner", "0");
 
-  await page.waitFor(500)
+      await page.waitFor(500);
 
-  const checkbox = await page.$('input[name="control.panel:control.checkbox"]');
-  await checkbox.click()
+      const checkbox = await page.$(
+        'input[name="control.panel:control.checkbox"]'
+      );
+      await checkbox.click();
 
-  await page.waitFor(1000)
+      await page.waitFor(1000);
 
-  const submit = await page.$('input[name="fortsatt"]');
-  await submit.click()
+      const submit = await page.$('input[name="fortsatt"]');
+      await submit.click();
 
-  await page.waitFor(1000)
+      await page.waitFor(1000);
 
-  var appointmentAvailability = ''
-  if ((await page.content()).match('Det finns inga mer lediga tider för närvarande.') !== null) {
-    appointmentAvailability = 'No Appointments Available'
-    console.log('No Appointments Available')
-  }
-  else {
-    appointmentAvailability = 'Appointments Available'
-    console.log('Appointments Available')
+      var appointmentAvailability = "";
+      if (
+        (await page.content()).match(
+          "Det finns inga mer lediga tider för närvarande."
+        ) !== null
+      ) {
+        appointmentAvailability = "No Appointments Available";
+        console.log("No Appointments Available");
+      } else {
+        appointmentAvailability = "Appointments Available";
+        console.log("Appointments Available");
+      }
 
-  }
-  
-  await page.waitFor(1000)
+      await page.waitFor(1000);
 
-  await browser.close();
+      await browser.close();
 
-  return appointmentAvailability;
-
-}
+      return resolve(appointmentAvailability);
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
 
 module.exports = PassportScraper;
